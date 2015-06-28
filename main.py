07effb5822dd6e8d4efdeccaa8237fbf95eaa802
Dylan Joss <djoss@mit.edu>
2015-06-28 02:59:42 -0700

    seed db with Old Man and the Sea

--- main.py
+++ main.py
@@ -22,5 +22,25 @@ def send_css(path):
     print path
     return send_from_directory('fonts', path)
 
+def setup_db():
+	if books.count() == 0:
+		book = {'author': 'Ernest Hemingway', 'title':'The Old Man and the Sea'}
+		books.insert_one(book)
+	if pages.count() == 0:
+		page_1 = {'book_id': '558fbdd7207d5be0f4f1c621',
+				  'url':'https://drive.google.com/file/d/0ByVj29dKGJcAcFk3Y0V6bFAyNWc/view?usp=sharing',
+				  'num_copies': 1}
+		page_2 = {'book_id': '558fbdd7207d5be0f4f1c621',
+				  'url': 'https://drive.google.com/file/d/0ByVj29dKGJcAaFdrbEc3VlkzR0U/view?usp=sharing',
+				  'num_copies': 1}
+		page_3 = {'book_id': '558fbdd7207d5be0f4f1c621',
+				  'url': 'https://drive.google.com/file/d/0ByVj29dKGJcAdFc0WG1PcExHeFU/view?usp=sharing',
+				  'num_copies': 1}
+		page_4 = {'book_id': '558fbdd7207d5be0f4f1c621',
+				  'url': 'https://drive.google.com/file/d/0ByVj29dKGJcAU3pXbXBLa2VfQ28/view?usp=sharing',
+				  'num_copies': 1}
+		pages.insert_many([page_1, page_2, page_3, page_4])
+
 if __name__ == "__main__":
+	setup_db()
 	app.run()
