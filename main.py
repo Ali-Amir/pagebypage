9f40b1c65a3d1e775d46f70f95944f84e029fbe6
Dylan Joss <djoss@mit.edu>
2015-06-28 03:10:03 -0700

    update book images links

--- main.py
+++ main.py
@@ -28,16 +28,16 @@ def setup_db():
 		books.insert_one(book)
 	if pages.count() == 0:
 		page_1 = {'book_id': '558fbdd7207d5be0f4f1c621',
-				  'url':'https://drive.google.com/file/d/0ByVj29dKGJcAcFk3Y0V6bFAyNWc/view?usp=sharing',
+				  'url':'https://raw.githubusercontent.com/Ali-Amir/pagebypage/master/books/558fbdd7207d5be0f4f1c621_1.jpg',
 				  'num_copies': 1}
 		page_2 = {'book_id': '558fbdd7207d5be0f4f1c621',
-				  'url': 'https://drive.google.com/file/d/0ByVj29dKGJcAaFdrbEc3VlkzR0U/view?usp=sharing',
+				  'url': 'https://raw.githubusercontent.com/Ali-Amir/pagebypage/master/books/558fbdd7207d5be0f4f1c621_2.jpg',
 				  'num_copies': 1}
 		page_3 = {'book_id': '558fbdd7207d5be0f4f1c621',
-				  'url': 'https://drive.google.com/file/d/0ByVj29dKGJcAdFc0WG1PcExHeFU/view?usp=sharing',
+				  'url': 'https://raw.githubusercontent.com/Ali-Amir/pagebypage/master/books/558fbdd7207d5be0f4f1c621_3.jpg',
 				  'num_copies': 1}
 		page_4 = {'book_id': '558fbdd7207d5be0f4f1c621',
-				  'url': 'https://drive.google.com/file/d/0ByVj29dKGJcAU3pXbXBLa2VfQ28/view?usp=sharing',
+				  'url': 'https://raw.githubusercontent.com/Ali-Amir/pagebypage/master/books/558fbdd7207d5be0f4f1c621_4.jpg',
 				  'num_copies': 1}
 		pages.insert_many([page_1, page_2, page_3, page_4])
 
